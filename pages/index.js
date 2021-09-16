2a49ee25f392a2df27dc7bba2d0b651e7b2fa1e0
enri19 <enripermana@gmail.com>
2021-09-17 02:20:18 +0700

    Edit sub

--- pages/index.js
+++ pages/index.js
@@ -227,6 +227,7 @@ class App extends React.Component{
     return (
       <div className={classes.root}>
         <h2 className={classes.textHead}>Perhitungan Suara Pilkades Sukaratu Tahun 2021</h2>
+<p>data ini tidak valid, data ini hanya sebagai bahan testing</p>
         <Grid container spacing={3}>
           <Grid item xs={12} sm={12} md={4} lg={3}>
             <Grid className={classes.candidateContent} container spacing={3}>
