265974d68d299d61c97e196a40716aec386c47c4
enri19 <enripermana@gmail.com>
2021-10-30 22:18:14 +0700

    Update index.js
    disabled

--- pages/user/index.js
+++ pages/user/index.js
@@ -1,51 +1,12 @@
 // import CreateUser from '../../components/CreateUser';
 
 // const User = () => {
-//   // const [users, setUsers] = useState([]);
-//   // useEffect(() => {
-//   //   fire.firestore()
-//   //     .collection('user')
-//   //     .onSnapshot(snap => {
-//   //       const users = snap.docs.map(doc => ({
-//   //         id: doc.id,
-//   //         ...doc.data()
-//   //       }));
-
-//   //       console.log(users);
-
-//   //       setUsers(users);
-//   //     });
-//   // }, []);
 
 //   return (
 //     <div>
-//       {/* <Head>
-//         <title>User</title>
-//       </Head>
-//       <h1>User</h1>
-//       <ul>
-//         {users.length !== 0 && users.map(user =>
-//           <li key={user.id}>
-//             {blog.userName}
-//           </li>
-//         )}
-//       </ul> */}
 //       <CreateUser />
 //     </div>
 //   )
 // }
 
 // export default User;
-
-import CreateUser from '../../components/CreateUser';
-
-const User = () => {
-
-  return (
-    <div>
-      <CreateUser />
-    </div>
-  )
-}
-
-export default User;
